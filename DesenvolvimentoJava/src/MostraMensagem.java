bae7edaffd07c596fdb8651871638359ec238f6a
naluhfvc <analufavacho@gmail.com>
2022-08-04 20:17:47 -0300

    update

---
+++ DesenvolvimentoJava/src/MostraMensagem.java
@@ -0,0 +1,15 @@
+
+public class MostraMensagem {
+	public static void main(String[] args) {
+
+		int num1 = 40;
+		int num2 = 10;
+
+		System.out.printf("\nO resultado da soma entre %d e %d igual a %d.", num1, num2, (num1 + num2));
+		System.out.printf("\nO resultado da subtracao entre %d e %d igual a %d.", num1, num2, (num1 - num2));
+		System.out.printf("\nO resultado da multiplicacao entre %d e %d  igual a %d.", num1, num2, (num1 * num2));
+		System.out.printf("\nO resultado da divisao entre %d e %d igual a %d.", num1, num2, (num1 / num2));
+
+	}
+
+}
